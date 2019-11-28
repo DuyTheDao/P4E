e5602a5b14a51ef00bf7b995ae3d911147b55cdb
Duy Dao <DuyDao@gmail.com>
2019-11-27 21:18:06 -0500

    Create 8_5.py

---
+++ 8_5.py
@@ -0,0 +1,18 @@
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+
+for line in fh:
+    if line.startswith("From "):
+        parse = line.rstrip().split()
+        emails = parse[1]
+        
+        count = count + 1
+        print(emails)
+    else:
+        continue
+        
+print("There were", count, "lines in the file with From as the first word")
